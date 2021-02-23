dfb837bed6f3ae8e26bdd5a5f70e5061ec6e545e
Nathan Sanford <nwsanfor24@gmail.com>
2021-02-23 10:29:43 -0500

    correct spelling errors

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ let clickTime = 0
 let timesClicked = 0
 
 loveMe.addEventListener('click', (e) => {
-    if (clickTime === 0) {
+    if(clickTime === 0) {
         clickTime = new Date().getTime()
     } else {
-        if ((new Date().getTime() - clickTime) < 800) {
+        if((new Date().getTime() - clickTime) < 800) {
             createHeart(e)
             clickTime = 0
         } else {
@@ -31,7 +31,7 @@ const createHeart = (e) => {
     const xInside = x - leftOffset
     const yInside = y - topOffset
 
-    heart.style.top `${yInside}px`
+    heart.style.top = `${yInside}px`
     heart.style.left = `${xInside}px`
 
     loveMe.appendChild(heart)
